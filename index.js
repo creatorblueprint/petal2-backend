5eff33d6591166eceaa938c84655ef7e44b45a49
creatorblueprint <192785251+creatorblueprint@users.noreply.github.com>
2026-02-23 11:46:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const User = require("./models/user");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash-lite" });
 
 
 // ====== CREATE APP ======
