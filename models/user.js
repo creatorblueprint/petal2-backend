751f7fd98ab83ab0c16c9944ca331f40de937fb5
creatorblueprint <192785251+creatorblueprint@users.noreply.github.com>
2026-02-24 05:06:39 +0530

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  messageCount: {
+    type: Number,
+    default: 0
+  },
+  lastReset: {
+    type: Date,
+    default: Date.now
+  },
+  plan: {
+    type: String,
+    default: "free"
+  },
+  
+  planExpiry: {
+    type: Date,
+    default: null
+  },
+  
+  usageToday: {
+    type: Number,
+    default: 0
+  },
+  
+  permanentMemoryLimit: {
+    type: Number,
+    default: 5
+  },
+  mood: {
+    type: String,
+    default: "soft"
+  },
+  memories: {
+    type: [
+      {
+        content: String,
+        createdAt: {
+          type: Date,
+          default: Date.now
+        }
+      }
+    ],
+    default: []
+  }
+
+});
+
+module.exports = mongoose.model("User", userSchema);
