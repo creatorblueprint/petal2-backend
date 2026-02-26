44e0b8a5276ebe2ac558cc826c8fbf5051f64284
creatorblueprint <192785251+creatorblueprint@users.noreply.github.com>
2026-02-27 00:34:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,8 +25,9 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const modelPriority = [
   "gemini-2.5-flash",
-  "gemini-2.5-pro",
-  "gemini-2.5-flash-lite"
+  "gemini-2.5-flash-lite",
+  "gemini-2.0-flash",
+  "gemini-2.0-flash-lite"
 ];
 
 
